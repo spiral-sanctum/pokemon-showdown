55898109193064101982e2943df93f35c93fa853
SDM0 <60699143+SDM0@users.noreply.github.com>
2023-10-01 01:24:41 +0200

    Actually lemme make this even cleaner

--- data/abilities.ts
+++ data/abilities.ts
@@ -5818,11 +5818,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		darkarts: {
 			onAllyBasePowerPriority: 22,
 			onAllyBasePower(basePower, attacker, defender, move) {
-				if (move.type === 'Ghost') {
-					this.debug('Dark Arts boost');
-					return this.chainModify(1.25);
-				}
-				else if (move.type === 'Dark') {
+				if (move.type === 'Ghost' || move.type === 'Dark') {
 					this.debug('Dark Arts boost');
 					return this.chainModify(1.25);
 				}
